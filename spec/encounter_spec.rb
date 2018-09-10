50ef56e2409817b3d1458ce8760b767defcbba08
audramatar <audra.matar@wpengine.com>
2018-09-10 15:18:13 -0500

    Removing unnecessary test.

--- spec/encounter_spec.rb
+++ spec/encounter_spec.rb
@@ -21,22 +21,4 @@ describe TrapEncounter do
 end
 
 describe TreasureEncounter do
-  context 'When creating a Treasure encounter' do
-    level = 1
-
-    it 'should have all of the normal normal when sending in no variation' do
-      encounter = TreasureEncounter.new(level)
-      expect(encounter.type).to eq('treasure, normal')
-    end
-
-    it 'should have all of the normal normal when sending in the normal variation' do
-      encounter = TreasureEncounter.new(level, 'normal')
-      expect(encounter.type).to eq('treasure, normal')
-    end
-
-    it 'should have the mimic type when sending in the mimic variation' do
-      encounter = TreasureEncounter.new(level, 'mimic')
-      expect(encounter.type).to eq('treasure, mimic')
-    end
-  end
 end
